17367413071ba5e18b133126bbd221aeafda3d9e
Yandi <yandivd@gmail.com>
2022-08-14 21:48:19 -0400

    testimonios terminados

--- main/views.py
+++ main/views.py
@@ -5,7 +5,11 @@ from .models import *
 def index(request):
 
     proyectos = Proyecto.objects.all()
+    testimonios = Testimoni.objects.all()
+    for i in testimonios:
+        print(i)
     data={
-        'proyectos': proyectos
+        'proyectos': proyectos,
+        'testimonios': testimonios,
     }
     return render(request, 'main/index.html', data)
