49f59505ea4f0764f7a90c7f028baba19bb599ca
Yandi <yandivd@gmail.com>
2022-08-14 21:21:58 -0400

    creado el model Testimonio

--- main/admin.py
+++ main/admin.py
@@ -4,5 +4,9 @@ from .models import *
 class ProyectoAdmin(admin.ModelAdmin):
     list_display = 'nombre', 'image', 'link', 'categoria'
 
+class TestimonioAdmin(admin.ModelAdmin):
+    list_display = 'nombre', 'ocupacion', 'image', 'texto'
+
 # Register your models here.
 admin.site.register(Proyecto, ProyectoAdmin)
+admin.site.register(Testimoni, TestimonioAdmin)
--- main/models.py
+++ main/models.py
@@ -13,3 +13,16 @@ class Proyecto(models.Model):
     class Meta:
         verbose_name = 'Proyecto'
         verbose_name_plural = 'Proyectos'
+
+class Testimoni(models.Model):
+    nombre = models.CharField(max_length=100)
+    ocupacion = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='testimonios', blank=True, null=True)
+    texto = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = 'Testimonio'
+        verbose_name_plural = 'Testimonios'
