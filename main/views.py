b9a56f8b969256e5280936c6c1e362ef3a3f8c03
yandivd <yandivd@gmail.com>
2022-08-22 20:58:07 -0400

    todo lindon

--- main/views.py
+++ main/views.py
@@ -70,7 +70,7 @@ class TestimonioCreateView(CreateView):
                               image=image['image'],
                               estado=estado)
             testi.save()
-            send_email('Nuevo testimonio', usuario, 'http://localhost:8000/listar/testimonios/')
+            send_email('Nuevo testimonio en myPortafolio', usuario, 'http://localhost:8000/listar/testimonios/')
             messages.success(request, "Muchas gracias por compartir su testimonio, una vez sea revisado se hara publico")
             return redirect('index')
         else:
@@ -104,7 +104,7 @@ def send_emailC(email,texto):
         #construir el mensaje
         mensaje= MIMEText("""Alguien ha hecho contacto contigo a trves de myPortafolio. 
         Entre al siguiente link para revisarlo: http://localhost:8000/listar/contactos/ """+
-                          "Correo: "+email+'/n'+texto )
+                          "Correo: "+email+' '+texto )
         mensaje['From'] = settings.EMAIL_HOST_USER
         mensaje['To'] = email_to
         mensaje['Subject'] = 'Contacto en myPortafolio'
