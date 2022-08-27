c7f71f9f506e4cfa1cb76c5fe0d1402c76404ec0
Yandi <yandivd@gmail.com>
2022-08-27 16:36:29 -0400

    sin drop

--- myPortafolio/settings.py
+++ myPortafolio/settings.py
@@ -168,7 +168,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #dropbox
 #
-# DROPBOX_OAUTH2_TOKEN = 'sl.BOKYY6Sg4RAcp5p9pX5Y5I87jB28JDzEOLyldnCtP7wCCFQExzkL2rr72rZl1Zu3-wO0AC4lGEBeUhPpm4qdSBS0mGX25PWSBDRxAGABdKypHhPSwE0bX6p0Fvf2re-SpbdOsdftUlIX'
+# DROPBOX_OAUTH2_TOKEN = 'U14eIr9d8roAAAAAAAAAAXVs9MsVFg1pWLLo5NuxD5oYoqNKJvN1yo8qjYyH1hOi'
 # DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 #
 # dbx = dropbox.Dropbox (DROPBOX_OAUTH2_TOKEN)
